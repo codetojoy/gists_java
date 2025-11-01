148c13bc5b42a82cfcf723a59f353066e3112867
Michael Easter <codetojoy@gmail.com>
2025-11-01 16:14:32 -0300

    incremental update Sat  1 Nov 2025 16:14:32 ADT

--- testcontainers_apr_2023/postgres/src/test/java/BasicTest.java
+++ testcontainers_apr_2023/postgres/src/test/java/BasicTest.java
@@ -2,15 +2,19 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 import static org.junit.Assert.*;
 
+@Testcontainers
 public class BasicTest {
+    @Container
+    private final GenericContainer<?> container = new GenericContainer<>(DockerImageName.parse("postgres"));
 
     @Test
     public void testCanary() {
-        final var container = new GenericContainer<>(DockerImageName.parse("postgres"));
         container.start();
         assertTrue(container.isRunning());
         container.stop();
