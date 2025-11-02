096ff30b83a1701a62d548418940c6c7bebe0150
Michael Easter <codetojoy@gmail.com>
2025-11-02 15:51:48 -0400

    new example with custom Python docker image

---
+++ testcontainers_apr_2023/python/README.md
@@ -0,0 +1,7 @@
+
+### notes
+
+* in Terminal 1, cd into `server` and build/run Docker image
+    * observe `simple-python-server` image in Docker
+    * confirm with http://localhost:8888
+* in Terminal 2, run `./test.sh` from this folder
---
+++ testcontainers_apr_2023/python/src/test/java/BasicTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Testcontainers
+public class BasicTest {
+    @Container
+    private final GenericContainer<?> container = new GenericContainer<>(DockerImageName.parse("simple-python-server"));
+
+    @Test
+    public void testCanary() {
+        assertTrue(container.isRunning());
+        System.out.println("TRACER container name: " + container.getContainerName());
+    }
+}
