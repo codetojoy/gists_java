4aea1d31c6d496269c387b272b3c9a70f06e4580
Michael Easter <codetojoy@gmail.com>
2025-11-03 21:09:21 -0400

    mapped port example

--- testcontainers_apr_2023/python/src/test/java/BasicTest.java
+++ testcontainers_apr_2023/python/src/test/java/BasicTest.java
@@ -8,12 +8,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @Testcontainers
 public class BasicTest {
+    private static final String IMAGE_NAME = "simple-python-server";
+    private static final int PORT = 8888;
+
     @Container
-    private final GenericContainer<?> container = new GenericContainer<>(DockerImageName.parse("simple-python-server"));
+    private final GenericContainer<?> container = new GenericContainer<>(DockerImageName.parse(IMAGE_NAME))
+                                                        .withExposedPorts(PORT);
 
     @Test
     public void testCanary() {
         assertTrue(container.isRunning());
-        System.out.println("TRACER container name: " + container.getContainerName());
+        System.out.println("TRACER container name: " + container.getContainerName()
+            + " on port " + container.getMappedPort(PORT));
     }
 }
